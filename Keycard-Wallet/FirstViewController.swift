843da5d5415525339aa928d5c1755fa96df034c2
Ediz Turcan <0x6564697a@turcan.de>
2019-11-03 09:49:18 +0100

    project created

---
+++ Keycard-Wallet/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Keycard-Wallet
+//
+//  Created by Ediz Turcan on 03.11.19.
+//  Copyright © 2019 b1ue.tech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Keycard-Wallet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Keycard-Wallet
+//
+//  Created by Ediz Turcan on 03.11.19.
+//  Copyright © 2019 b1ue.tech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
